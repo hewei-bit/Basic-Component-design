0f8d0c9a038426ac242a463a4bf9db0e28a61143
hewei-bit <1003826976@qq.com>
2022-02-24 00:05:36 +0800

    新建

--- 3.1池式组件/3.1.1手写线程池与性能分析/src/practice/thread_pool_hw.c
+++ 3.1池式组件/3.1.1手写线程池与性能分析/src/practice/thread_pool_hw.c
@@ -11,3 +11,16 @@
         item->next = list; \
         list = item;       \
     } while (0)
+
+//使用宏定义将某个东西从队列中去除
+#define LL_REMOVE(item, list)              \
+    do                                     \
+    {                                      \
+        if (item->prev != NULL)            \
+            item->prev->next = item->next; \
+        if (item->next != NULL)            \
+            item->next->prev = item->prev; \
+        if (list == item)                  \
+            list = item->next;             \
+        item->prev = item->next = NULL;    \
+    } while (0)
