49b54a01133afa4fee2ef57c7f00d01d1f1e5283
hewei-bit <1003826976@qq.com>
2022-02-24 17:24:38 +0800

    线程池

--- 3.1池式组件/3.1.1手写线程池与性能分析/src/practice/thread_pool_hw.c
+++ 3.1池式组件/3.1.1手写线程池与性能分析/src/practice/thread_pool_hw.c
@@ -24,3 +24,63 @@
             list = item->next;             \
         item->prev = item->next = NULL;    \
     } while (0)
+
+// Nworker线程节点
+typedef struct NWORKER
+{
+    pthread_t thread;
+    int terminate;
+
+} nworker;
+
+// Njob任务节点
+typedef struct NJOB
+{
+} nJob;
+
+// Nworkqueue 线程队列
+typedef struct NWORKQUEUE
+{
+    struct NWORKER *workers;
+} nWorkQueue;
+
+//给队列起别名为线程池
+typedef nWorkQueue nThreadPool;
+
+//给线程添加一个任务
+static void *ntyWorkerThread(void *ptr)
+{
+}
+
+//创建线程池
+int ntyThreadPoolCreate(nThreadPool *workqueue, int numWorkers)
+{
+}
+
+//关闭线程池
+void ntyThreadPoolShutdown(nThreadPool *workqueue)
+{
+}
+
+//将任务添加进等待队列
+void ntyThreadPoolQueue(nThreadPool *workqueue, nJob *job)
+{
+
+    // 发送一个信号给另外一个正在处于阻塞等待状态的线程,使其脱离阻塞状态,继续执行
+}
+
+/************************** debug thread pool **************************/
+// sdk  --> software develop kit
+//  提供SDK给其他开发者使用
+#if 1
+
+#define KING_MAX_THREAD 80
+#define KING_COUNTER_SIZE 1000
+void king_counter(nJob *job)
+{
+}
+
+int main(int argc, char *argv[])
+{
+}
+#endif
