1e38b150e8ae887ffbecf5f596cdd6a5e201e627
hewei-bit <1003826976@qq.com>
2022-03-05 18:07:03 +0800

    内存池

--- 3.1池式组件/3.1.2内存池的实现与场景分析/src/example/mmpool.c
+++ 3.1池式组件/3.1.2内存池的实现与场景分析/src/example/mmpool.c
@@ -1,6 +1,5 @@
 
 
-
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
@@ -8,42 +7,36 @@
 
 #include <fcntl.h>
 
+#define MP_ALIGNMENT 32
+#define MP_PAGE_SIZE 4096
+#define MP_MAX_ALLOC_FROM_POOL (MP_PAGE_SIZE - 1)
 
+#define mp_align(n, alignment) (((n) + (alignment - 1)) & ~(alignment - 1))
+#define mp_align_ptr(p, alignment) (void *)((((size_t)p) + (alignment - 1)) & ~(alignment - 1))
 
-#define MP_ALIGNMENT       		32
-#define MP_PAGE_SIZE			4096
-#define MP_MAX_ALLOC_FROM_POOL	(MP_PAGE_SIZE-1)
-
-#define mp_align(n, alignment) (((n)+(alignment-1)) & ~(alignment-1))
-#define mp_align_ptr(p, alignment) (void *)((((size_t)p)+(alignment-1)) & ~(alignment-1))
-
-
-
-
-
-struct mp_large_s {
-	struct mp_large_s *next;
-	void *alloc;
+struct mp_large_s
+{
+    struct mp_large_s *next;
+    void *alloc;
 };
 
-struct mp_node_s {
+struct mp_node_s
+{
+    unsigned char *last;
+    unsigned char *end;
 
-	unsigned char *last;
-	unsigned char *end;
-	
-	struct mp_node_s *next;
-	size_t failed;
+    struct mp_node_s *next;
+    size_t failed;
 };
 
-struct mp_pool_s {
+struct mp_pool_s
+{
+    size_t max;
 
-	size_t max;
+    struct mp_node_s *current;
+    struct mp_large_s *large;
 
-	struct mp_node_s *current;
-	struct mp_large_s *large;
-
-	struct mp_node_s head[0];
-
+    struct mp_node_s head[0];
 };
 
 struct mp_pool_s *mp_create_pool(size_t size);
@@ -53,283 +46,299 @@ void *mp_nalloc(struct mp_pool_s *pool, size_t size);
 void *mp_calloc(struct mp_pool_s *pool, size_t size);
 void mp_free(struct mp_pool_s *pool, void *p);
 
+struct mp_pool_s *mp_create_pool(size_t size)
+{
+    struct mp_pool_s *p;
+    int ret = posix_memalign((void **)&p, MP_ALIGNMENT, size + sizeof(struct mp_pool_s) + sizeof(struct mp_node_s));
+    if (ret)
+    {
+        return NULL;
+    }
 
-struct mp_pool_s *mp_create_pool(size_t size) {
+    p->max = (size < MP_MAX_ALLOC_FROM_POOL) ? size : MP_MAX_ALLOC_FROM_POOL;
+    p->current = p->head;
+    p->large = NULL;
 
-	struct mp_pool_s *p;
-	int ret = posix_memalign((void **)&p, MP_ALIGNMENT, size + sizeof(struct mp_pool_s) + sizeof(struct mp_node_s));
-	if (ret) {
-		return NULL;
-	}
-	
-	p->max = (size < MP_MAX_ALLOC_FROM_POOL) ? size : MP_MAX_ALLOC_FROM_POOL;
-	p->current = p->head;
-	p->large = NULL;
+    p->head->last = (unsigned char *)p + sizeof(struct mp_pool_s) + sizeof(struct mp_node_s);
+    p->head->end = p->head->last + size;
 
-	p->head->last = (unsigned char *)p + sizeof(struct mp_pool_s) + sizeof(struct mp_node_s);
-	p->head->end = p->head->last + size;
+    p->head->failed = 0;
 
-	p->head->failed = 0;
-
-	return p;
-
+    return p;
 }
 
-void mp_destory_pool(struct mp_pool_s *pool) {
+void mp_destory_pool(struct mp_pool_s *pool)
+{
 
-	struct mp_node_s *h, *n;
-	struct mp_large_s *l;
+    struct mp_node_s *h, *n;
+    struct mp_large_s *l;
 
-	for (l = pool->large; l; l = l->next) {
-		if (l->alloc) {
-			free(l->alloc);
-		}
-	}
+    for (l = pool->large; l; l = l->next)
+    {
+        if (l->alloc)
+        {
+            free(l->alloc);
+        }
+    }
 
-	h = pool->head->next;
+    h = pool->head->next;
 
-	while (h) {
-		n = h->next;
-		free(h);
-		h = n;
-	}
+    while (h)
+    {
+        n = h->next;
+        free(h);
+        h = n;
+    }
 
-	free(pool);
-
+    free(pool);
 }
 
-void mp_reset_pool(struct mp_pool_s *pool) {
+void mp_reset_pool(struct mp_pool_s *pool)
+{
 
-	struct mp_node_s *h;
-	struct mp_large_s *l;
+    struct mp_node_s *h;
+    struct mp_large_s *l;
 
-	for (l = pool->large; l; l = l->next) {
-		if (l->alloc) {
-			free(l->alloc);
-		}
-	}
+    for (l = pool->large; l; l = l->next)
+    {
+        if (l->alloc)
+        {
+            free(l->alloc);
+        }
+    }
 
-	pool->large = NULL;
+    pool->large = NULL;
 
-	for (h = pool->head; h; h = h->next) {
-		h->last = (unsigned char *)h + sizeof(struct mp_node_s);
-	}
-
+    for (h = pool->head; h; h = h->next)
+    {
+        h->last = (unsigned char *)h + sizeof(struct mp_node_s);
+    }
 }
 
-static void *mp_alloc_block(struct mp_pool_s *pool, size_t size) {
+static void *mp_alloc_block(struct mp_pool_s *pool, size_t size)
+{
 
-	unsigned char *m;
-	struct mp_node_s *h = pool->head;
-	size_t psize = (size_t)(h->end - (unsigned char *)h);
-	
-	int ret = posix_memalign((void **)&m, MP_ALIGNMENT, psize);
-	if (ret) return NULL;
+    unsigned char *m;
+    struct mp_node_s *h = pool->head;
+    size_t psize = (size_t)(h->end - (unsigned char *)h);
 
-	struct mp_node_s *p, *new_node, *current;
-	new_node = (struct mp_node_s*)m;
+    int ret = posix_memalign((void **)&m, MP_ALIGNMENT, psize);
+    if (ret)
+        return NULL;
 
-	new_node->end = m + psize;
-	new_node->next = NULL;
-	new_node->failed = 0;
+    struct mp_node_s *p, *new_node, *current;
+    new_node = (struct mp_node_s *)m;
 
-	m += sizeof(struct mp_node_s);
-	m = mp_align_ptr(m, MP_ALIGNMENT);
-	new_node->last = m + size;
+    new_node->end = m + psize;
+    new_node->next = NULL;
+    new_node->failed = 0;
 
-	current = pool->current;
+    m += sizeof(struct mp_node_s);
+    m = mp_align_ptr(m, MP_ALIGNMENT);
+    new_node->last = m + size;
 
-	for (p = current; p->next; p = p->next) {
-		if (p->failed++ > 4) {
-			current = p->next;
-		}
-	}
-	p->next = new_node;
+    current = pool->current;
 
-	pool->current = current ? current : new_node;
+    for (p = current; p->next; p = p->next)
+    {
+        if (p->failed++ > 4)
+        {
+            current = p->next;
+        }
+    }
+    p->next = new_node;
 
-	return m;
+    pool->current = current ? current : new_node;
 
+    return m;
 }
 
-static void *mp_alloc_large(struct mp_pool_s *pool, size_t size) {
+static void *mp_alloc_large(struct mp_pool_s *pool, size_t size)
+{
 
-	void *p = malloc(size);
-	if (p == NULL) return NULL;
+    void *p = malloc(size);
+    if (p == NULL)
+        return NULL;
 
-	size_t n = 0;
-	struct mp_large_s *large;
-	for (large = pool->large; large; large = large->next) {
-		if (large->alloc == NULL) {
-			large->alloc = p;
-			return p;
-		}
-		if (n ++ > 3) break;
-	}
+    size_t n = 0;
+    struct mp_large_s *large;
+    for (large = pool->large; large; large = large->next)
+    {
+        if (large->alloc == NULL)
+        {
+            large->alloc = p;
+            return p;
+        }
+        if (n++ > 3)
+            break;
+    }
 
-	large = mp_alloc(pool, sizeof(struct mp_large_s));
-	if (large == NULL) {
-		free(p);
-		return NULL;
-	}
+    large = mp_alloc(pool, sizeof(struct mp_large_s));
+    if (large == NULL)
+    {
+        free(p);
+        return NULL;
+    }
 
-	large->alloc = p;
-	large->next = pool->large;
-	pool->large = large;
+    large->alloc = p;
+    large->next = pool->large;
+    pool->large = large;
 
-	return p;
+    return p;
 }
 
-void *mp_memalign(struct mp_pool_s *pool, size_t size, size_t alignment) {
+void *mp_memalign(struct mp_pool_s *pool, size_t size, size_t alignment)
+{
 
-	void *p;
-	
-	int ret = posix_memalign(&p, alignment, size);
-	if (ret) {
-		return NULL;
-	}
+    void *p;
 
-	struct mp_large_s *large = mp_alloc(pool, sizeof(struct mp_large_s));
-	if (large == NULL) {
-		free(p);
-		return NULL;
-	}
+    int ret = posix_memalign(&p, alignment, size);
+    if (ret)
+    {
+        return NULL;
+    }
 
-	large->alloc = p;
-	large->next = pool->large;
-	pool->large = large;
+    struct mp_large_s *large = mp_alloc(pool, sizeof(struct mp_large_s));
+    if (large == NULL)
+    {
+        free(p);
+        return NULL;
+    }
 
-	return p;
+    large->alloc = p;
+    large->next = pool->large;
+    pool->large = large;
+
+    return p;
 }
 
+void *mp_alloc(struct mp_pool_s *pool, size_t size)
+{
 
+    unsigned char *m;
+    struct mp_node_s *p;
 
+    if (size <= pool->max)
+    {
+        p = pool->current;
+        do
+        {
+            m = mp_align_ptr(p->last, MP_ALIGNMENT);
+            if ((size_t)(p->end - m) >= size)
+            {
+                p->last = m + size;
+                return m;
+            }
+            p = p->next;
+        } while (p);
+        return mp_alloc_block(pool, size);
+    }
 
-void *mp_alloc(struct mp_pool_s *pool, size_t size) {
-
-	unsigned char *m;
-	struct mp_node_s *p;
-
-	if (size <= pool->max) {
-
-		p = pool->current;
-
-		do {
-			
-			m = mp_align_ptr(p->last, MP_ALIGNMENT);
-			if ((size_t)(p->end - m) >= size) {
-				p->last = m + size;
-				return m;
-			}
-			p = p->next;
-		} while (p);
-
-		return mp_alloc_block(pool, size);
-	}
-
-	return mp_alloc_large(pool, size);
-	
+    return mp_alloc_large(pool, size);
 }
 
+void *mp_nalloc(struct mp_pool_s *pool, size_t size)
+{
+    unsigned char *m;
+    struct mp_node_s *p;
 
-void *mp_nalloc(struct mp_pool_s *pool, size_t size) {
+    if (size <= pool->max)
+    {
+        p = pool->current;
 
-	unsigned char *m;
-	struct mp_node_s *p;
+        do
+        {
+            m = p->last;
+            if ((size_t)(p->end - m) >= size)
+            {
+                p->last = m + size;
+                return m;
+            }
+            p = p->next;
+        } while (p);
 
-	if (size <= pool->max) {
-		p = pool->current;
+        return mp_alloc_block(pool, size);
+    }
 
-		do {
-			m = p->last;
-			if ((size_t)(p->end - m) >= size) {
-				p->last = m+size;
-				return m;
-			}
-			p = p->next;
-		} while (p);
-
-		return mp_alloc_block(pool, size);
-	}
-
-	return mp_alloc_large(pool, size);
-	
+    return mp_alloc_large(pool, size);
 }
 
-void *mp_calloc(struct mp_pool_s *pool, size_t size) {
+void *mp_calloc(struct mp_pool_s *pool, size_t size)
+{
+    void *p = mp_alloc(pool, size);
+    if (p)
+    {
+        memset(p, 0, size);
+    }
 
-	void *p = mp_alloc(pool, size);
-	if (p) {
-		memset(p, 0, size);
-	}
-
-	return p;
-	
+    return p;
 }
 
-void mp_free(struct mp_pool_s *pool, void *p) {
+void mp_free(struct mp_pool_s *pool, void *p)
+{
+    struct mp_large_s *l;
+    for (l = pool->large; l; l = l->next)
+    {
+        if (p == l->alloc)
+        {
+            free(l->alloc);
+            l->alloc = NULL;
 
-	struct mp_large_s *l;
-	for (l = pool->large; l; l = l->next) {
-		if (p == l->alloc) {
-			free(l->alloc);
-			l->alloc = NULL;
-
-			return ;
-		}
-	}
-	
+            return;
+        }
+    }
 }
 
+int main(int argc, char *argv[])
+{
+    int size = 1 << 12;
 
-int main(int argc, char *argv[]) {
+    struct mp_pool_s *p = mp_create_pool(size);
 
-	int size = 1 << 12;
+    int i = 0;
+    for (i = 0; i < 10; i++)
+    {
 
-	struct mp_pool_s *p = mp_create_pool(size);
+        void *mp = mp_alloc(p, 512);
+        //		mp_free(mp);
+    }
 
-	int i = 0;
-	for (i = 0;i < 10;i ++) {
+    // printf("mp_create_pool: %ld\n", p->max);
+    printf("mp_align(123, 32): %d, mp_align(17, 32): %d\n", mp_align(24, 32), mp_align(17, 32));
+    // printf("mp_align_ptr(p->current, 32): %lx, p->current: %lx, mp_align(p->large, 32): %lx, p->large: %lx\n", mp_align_ptr(p->current, 32), p->current, mp_align_ptr(p->large, 32), p->large);
 
-		void *mp = mp_alloc(p, 512);
-//		mp_free(mp);
-	}
+    int j = 0;
+    for (i = 0; i < 5; i++)
+    {
 
-	//printf("mp_create_pool: %ld\n", p->max);
-	printf("mp_align(123, 32): %d, mp_align(17, 32): %d\n", mp_align(24, 32), mp_align(17, 32));
-	//printf("mp_align_ptr(p->current, 32): %lx, p->current: %lx, mp_align(p->large, 32): %lx, p->large: %lx\n", mp_align_ptr(p->current, 32), p->current, mp_align_ptr(p->large, 32), p->large);
+        char *pp = mp_calloc(p, 32);
+        for (j = 0; j < 32; j++)
+        {
+            if (pp[j])
+            {
+                printf("calloc wrong\n");
+            }
+            printf("calloc success\n");
+        }
+    }
 
-	int j = 0;
-	for (i = 0;i < 5;i ++) {
+    // printf("mp_reset_pool\n");
 
-		char *pp = mp_calloc(p, 32);
-		for (j = 0;j < 32;j ++) {
-			if (pp[j]) {
-				printf("calloc wrong\n");
-			}
-			printf("calloc success\n");
-		}
-	}
+    for (i = 0; i < 5; i++)
+    {
+        void *l = mp_alloc(p, 8192);
+        mp_free(p, l);
+    }
 
-	//printf("mp_reset_pool\n");
+    mp_reset_pool(p);
 
-	for (i = 0;i < 5;i ++) {
-		void *l = mp_alloc(p, 8192);
-		mp_free(p, l);
-	}
+    // printf("mp_destory_pool\n");
+    for (i = 0; i < 58; i++)
+    {
+        mp_alloc(p, 256);
+    }
 
-	mp_reset_pool(p);
+    mp_destory_pool(p);
 
-	//printf("mp_destory_pool\n");
-	for (i = 0;i < 58;i ++) {
-		mp_alloc(p, 256);
-	}
-
-	mp_destory_pool(p);
-
-	return 0;
-
+    return 0;
 }
-
-
-
---
+++ 3.1池式组件/3.1.2内存池的实现与场景分析/src/practice/mmpool_hw.c
@@ -0,0 +1,15 @@
+/**
+ * @File Name: mmpool_hw.c
+ * @Brief : ÊÖÐ´ÄÚ´æ³Ø¼Ó×¢ÊÍ
+ * @Author : hewei (hewei_1996@qq.com)
+ * @Version : 1.0
+ * @Creat Date : 2022-03-05
+ *
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+#include <fcntl.h>
