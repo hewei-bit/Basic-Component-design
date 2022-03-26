1bc960fb68edd826fe454745081dc610e7f6e983
hewei-bit <1003826976@qq.com>
2022-03-26 21:50:26 +0800

    原子操作与锁

--- 3.2高性能组件/3.2.1原子操作CAS与锁实现/src/example/lock.c
+++ 3.2高性能组件/3.2.1原子操作CAS与锁实现/src/example/lock.c
@@ -1,10 +1,8 @@
 
 
-
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-
 
 #define __USE_GNU
 #include <sched.h>
@@ -15,205 +13,194 @@
 
 #include <sys/syscall.h>
 
-#define THREAD_COUNT		3
-
+#define THREAD_COUNT 3
 
 pthread_mutex_t mutex;
 pthread_spinlock_t spinlock;
 
+int inc(int *value, int add)
+{
 
-int inc(int *value, int add) {
-
-	int old;
-	__asm__ volatile (
-		"lock; xaddl %2, %1;"
-		: "=a" (old)
-		: "m" (*value), "a" (add)
-		: "cc", "memory"
-	);
-	return old;
-
+    int old;
+    __asm__ volatile(
+        "lock; xaddl %2, %1;"
+        : "=a"(old)
+        : "m"(*value), "a"(add)
+        : "cc", "memory");
+    return old;
 }
 
+void *func(void *arg)
+{
 
+    int *pcount = (int *)arg;
 
-void *func(void *arg) {
+    int i = 0;
 
-	int *pcount = (int *)arg;
+    //
 
-	int i = 0;
-
-	//
-	
-	while (i ++ < 100000) {
+    while (i++ < 100000)
+    {
 #if 0
 		(*pcount) ++;
 #elif 0
-// 
-		pthread_mutex_lock(&mutex);
-		(*pcount) ++;
-		pthread_mutex_unlock(&mutex);
+
+        pthread_mutex_lock(&mutex);
+        (*pcount)++;
+        pthread_mutex_unlock(&mutex);
 
 #elif 0
 
-		pthread_spin_lock(&spinlock);
-		(*pcount) ++;
-		pthread_spin_unlock(&spinlock);
+        pthread_spin_lock(&spinlock);
+        (*pcount)++;
+        pthread_spin_unlock(&spinlock);
 
 #else
 
-		inc(pcount, 1);
+        inc(pcount, 1);
 
 #endif
-		usleep(1);
-
-	}
-	
-
+        usleep(1);
+    }
 }
-
 
 pthread_key_t key;
 
 typedef void *(*thread_cb)(void *);
 
+void print_thread1_key(void)
+{
 
-void print_thread1_key(void) {
+    int *p = (int *)pthread_getspecific(key);
 
-	int *p = (int *)pthread_getspecific(key);
-
-	printf("thread 1 : %d\n", *p);
-
+    printf("thread 1 : %d\n", *p);
 }
 
-void *thread1_proc(void *arg) {
+void *thread1_proc(void *arg)
+{
 
-	int i = 5;	
-	pthread_setspecific(key, &i);
+    int i = 5;
+    pthread_setspecific(key, &i);
 
-	print_thread1_key();
-
+    print_thread1_key();
 }
 
-void print_thread2_key(void) {
+void print_thread2_key(void)
+{
 
-	char *ptr = (char *)pthread_getspecific(key);
+    char *ptr = (char *)pthread_getspecific(key);
 
-	printf("thread 2 : %s\n", ptr);
-
+    printf("thread 2 : %s\n", ptr);
 }
 
+void *thread2_proc(void *arg)
+{
 
-
-void *thread2_proc(void *arg) {
-
-	char *ptr = "thread2_proc";
-	pthread_setspecific(key, ptr);
-	print_thread2_key();
-	
+    char *ptr = "thread2_proc";
+    pthread_setspecific(key, ptr);
+    print_thread2_key();
 }
 
-
-struct pair {
-	int x;
-	int y;
+struct pair
+{
+    int x;
+    int y;
 };
 
-void print_thread3_key(void) {
+void print_thread3_key(void)
+{
 
-	struct pair *p = (struct pair *)pthread_getspecific(key);
+    struct pair *p = (struct pair *)pthread_getspecific(key);
 
-	printf("thread 3  x: %d, y: %d\n", p->x, p->y);
-
+    printf("thread 3  x: %d, y: %d\n", p->x, p->y);
 }
 
+void *thread3_proc(void *arg)
+{
 
-void *thread3_proc(void *arg) {
+    struct pair p = {1, 2};
 
-	struct pair p = {1, 2};
-
-	pthread_setspecific(key, &p);
-	print_thread3_key();
-
+    pthread_setspecific(key, &p);
+    print_thread3_key();
 }
-
 
 // setjmp / longjmp
 
-struct ExceptionFrame {
+struct ExceptionFrame
+{
 
-	jmp_buf env;
-	int count;
+    jmp_buf env;
+    int count;
 
-	struct ExceptionFrame *next;
-
+    struct ExceptionFrame *next;
 };
 
 // stack --> current node save
 
-
-
-
-
 jmp_buf env;
 int count = 0;
 
+#define Try              \
+    count = setjmp(env); \
+    if (count == 0)
 
-#define Try 	count = setjmp(env); if (count == 0) 
+#define Catch(type) else if (count == type)
 
-#define Catch(type)  else if (count == type) 
+#define Throw(type) longjmp(env, type);
 
-#define Throw(type)	  longjmp(env, type);
+#define Finally
 
-#define Finally			
+void sub_func(int idx)
+{
 
-void sub_func(int idx) {
+    printf("sub_func : %d\n", idx);
+    // longjmp(env, idx);
 
-	printf("sub_func : %d\n", idx);
-	//longjmp(env, idx);
-
-	Throw(idx);
-
+    Throw(idx);
 }
 
+void process_affinity(int num)
+{
 
-void process_affinity(int num) {
+    // gettid();
+    pid_t selfid = syscall(__NR_gettid);
 
-	//gettid();
-	pid_t selfid = syscall(__NR_gettid);
+    cpu_set_t mask;
+    CPU_ZERO(&mask);
 
-	cpu_set_t mask;
-	CPU_ZERO(&mask);
+    CPU_SET(1, &mask);
 
-	CPU_SET(1, &mask);
+    // selfid
+    sched_setaffinity(0, sizeof(mask), &mask);
 
-	//selfid
-	sched_setaffinity(0, sizeof(mask), &mask);
-
-	while(1) ;
+    while (1)
+        ;
 }
 
+int main()
+{
 
-int main() {
+    // 8
+    int num = sysconf(_SC_NPROCESSORS_CONF);
 
-	// 8
-	int num = sysconf(_SC_NPROCESSORS_CONF);
+    int i = 0;
+    pid_t pid = 0;
+    for (i = 0; i < num / 2; i++)
+    {
+        pid = fork();
+        if (pid <= (pid_t)0)
+        {
+            break;
+        }
+    }
 
-	int i = 0;
-	pid_t pid = 0;
-	for (i = 0;i < num/2;i ++) {
-		pid = fork();
-		if (pid <= (pid_t)0) {
-			break;
-		}
-	}
+    if (pid == 0)
+    {
+        process_affinity(num);
+    }
 
-	if (pid == 0) {
-		process_affinity(num);
-	}
-
-	while (1) usleep(1);
-
+    while (1)
+        usleep(1);
 
 #if 0
 	count = setjmp(env);
@@ -233,38 +220,41 @@ int main() {
 	}
 #elif 0
 
-	Try {
+    Try
+    {
 
-		sub_func(++count);
-		
-	} Catch (1) {
+        sub_func(++count);
+    }
+    Catch(1)
+    {
 
-		sub_func(++count);
+        sub_func(++count);
+    }
+    Catch(2)
+    {
 
-	} Catch (2) {
+        sub_func(++count);
+    }
+    Catch(3)
+    {
 
-		sub_func(++count);
+        sub_func(++count);
+    }
+    Finally
+    {
 
-	} Catch (3) {
-
-		sub_func(++count);
-
-	} Finally {
-
-		printf("other item\n");
-
-	}
+        printf("other item\n");
+    }
 
 #endif
-	
 
-	pthread_t thid[THREAD_COUNT] = {0};
-	int count = 0;
+    pthread_t thid[THREAD_COUNT] = {0};
+    int count = 0;
 
-	pthread_mutex_init(&mutex, NULL);
-	pthread_spin_init(&spinlock, PTHREAD_PROCESS_SHARED);
+    pthread_mutex_init(&mutex, NULL);
+    pthread_spin_init(&spinlock, PTHREAD_PROCESS_SHARED);
 
-	pthread_key_create(&key, NULL);
+    pthread_key_create(&key, NULL);
 
 #if 0
 	int i = 0;
@@ -295,10 +285,5 @@ int main() {
 	}
 #endif
 
-	 
-	
-
-	return 0;
+    return 0;
 }
-
-
--- 3.2高性能组件/3.2.1原子操作CAS与锁实现/src/practice/lock_hw.c
+++ 3.2高性能组件/3.2.1原子操作CAS与锁实现/src/practice/lock_hw.c
@@ -6,3 +6,79 @@
  * @Creat Date : 2022-03-22
  *
  */
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define __USE_GNU
+#include <sched.h>
+
+#include <unistd.h>
+#include <pthread.h>
+#include <setjmp.h>
+
+#include <sys/syscall.h>
+
+#define TRHEAD_COUNT 3
+
+#define thread_single 1
+#define with_nothing 1
+#define with_mutex_lock 0
+#define with_spin_lock 0
+#define with_atomic 0
+
+pthread_mutex_t mutex;
+pthread_spinlock_t spinlock;
+
+// Ô­×Ó²Ù×÷
+int inc(int *value, int add)
+{
+    int old;
+    __asm__ volatile(
+        "lock; xaddl %2, %1;"
+        : "=a"(old)
+        : "m"(*value), "a"(add)
+        : "cc", "memory");
+    return old;
+}
+
+void *func(void *arg)
+{
+    int *pcount = (int *)arg;
+
+    int i = 0;
+
+    while (i++ < 100000)
+    {
+#if with_nothing
+        (*pcount)++;
+#elif with_mutex_lock
+
+#elif with_spin_lock
+
+#elif with_atomic
+
+#endif
+        usleep(1);
+    }
+}
+
+int main()
+{
+    pthread_t thid[TRHEAD_COUNT] = {0};
+
+    int count = 0;
+
+    pthread_mutex_init(&mutex, NULL);
+    pthread_spin_init(&spinlock, PTHREAD_PROCESS_SHARED);
+
+#if thread_single
+    int i = 0;
+    for (i = 0; i < TRHEAD_COUNT; i++)
+    {
+        pthread_create(&thid[i], NULL, func, &count);
+    }
+
+#endif
+}
