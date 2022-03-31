9920de6b9bad25886066fb823c90d2226c584cb6
hewei-bit <1003826976@qq.com>
2022-03-31 20:19:21 +0800

    定时器

--- 3.2高性能组件/3.2.2定时器方案红黑树，时间轮，最小堆/timers_src/practice/minheap/mh-timer_hw.h
+++ 3.2高性能组件/3.2.2定时器方案红黑树，时间轮，最小堆/timers_src/practice/minheap/mh-timer_hw.h
@@ -24,17 +24,10 @@ static uint32_t
 current_time()
 {
     uint32_t t;
-#if !defined(__APPLE__) || defined(AVAILABLE_MAC_OS_X_VERSION_10_12_AND_LATER)
     struct timespec ti;
     clock_gettime(CLOCK_MONOTONIC, &ti);
     t = (uint32_t)ti.tv_sec * 1000;
     t += ti.tv_nsec / 1000000;
-#else
-    struct timeval tv;
-    gettimeofday(&tv, NULL);
-    t = (uint32_t)tv.tv_sec * 1000;
-    t += tv.tv_usec / 1000;
-#endif
     return t;
 }
 
@@ -45,53 +38,18 @@ void init_timer()
 
 timer_entry_t *add_timer(uint32_t msec, timer_handler_pt callback)
 {
-    timer_entry_t *te = (timer_entry_t *)malloc(sizeof(*te));
-    if (!te)
-    {
-        return NULL;
-    }
-    memset(te, 0, sizeof(timer_entry_t));
-
-    te->handler = callback;
-    te->time = current_time() + msec;
-
-    if (0 != min_heap_push_(&min_heap, te))
-    {
-        free(te);
-        return NULL;
-    }
-    printf("add timer time = %u now = %u\n", te->time, current_time());
-    return te;
 }
 
 bool del_timer(timer_entry_t *e)
 {
-    return 0 == min_heap_erase_(&min_heap, e);
 }
 
 int find_nearest_expire_timer()
 {
-    timer_entry_t *te = min_heap_top_(&min_heap);
-    if (!te)
-        return -1;
-    int diff = (int)te->time - (int)current_time();
-    return diff > 0 ? diff : 0;
 }
 
 void expire_timer()
 {
-    uint32_t cur = current_time();
-    for (;;)
-    {
-        timer_entry_t *te = min_heap_top_(&min_heap);
-        if (!te)
-            break;
-        if (te->time > cur)
-            break;
-        te->handler(te);
-        min_heap_pop_(&min_heap);
-        free(te);
-    }
 }
 
 #endif // MARK_MINHEAP_TIMER_H
